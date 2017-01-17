88bbe7c8ef8232f91f34f60fdb5d419ff1fad48f
Elgskred <christoffer.antonsen@gmail.com>
2017-01-17 22:20:59 +0100

    -

--- client/views/List.js
+++ client/views/List.js
@@ -54,7 +54,7 @@ class List extends React.Component {
 		console.log("getting");
 		$.ajax ({
 	      method: 'GET',
-	      url: "http://localhost:3333/search/" + this.props.location.query.q,
+	      url: "http://awesomesauce-gaming.net:3333/search/" + this.props.location.query.q,
 	      success: (data) => {
 	        console.log(data);
 	        this.setState({searchResults: data[0]});
