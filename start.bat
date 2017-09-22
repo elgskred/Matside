cb3967cac9231a17a60735a4cc944a397165115c
Elgskred <christoffer.antonsen@gmail.com>
2017-09-22 10:13:33 +0200

    -

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
-cd C:\Users\Serveren\Documents\Matside\client
-start npm run test
+cd C:\Users\chris\Documents\GitHub\Matside\client
+start npm start
 
-cd C:\Users\Serveren\Documents\Matside\server
+cd C:\Users\chris\Documents\GitHub\Matside\server
 start npm start
