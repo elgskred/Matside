d1e66d3e9ac2e4cdae0bd113452228af31c957ee
Mathias <mathiassommerstad@gmail.com>
2017-10-05 12:52:38 +0200

    a

---
+++ client/components/DropdownMenuTest.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router';
+import { hashHistory } from 'react-router';
+
+let ReactCSSTransitionGroup = React.addons.CSSTransitionGroup
+
+class DropDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.state = {
+      menuActive: false
+    };
+  }
+
+  toggleMenu() {
+    let menuState = !this.state.menuActive;
+    this.setState({
+      menuActive: menuState
+    });
+  }
+
+  render() {
+    let menu;
+    if(this.state.menuActive) {
+      menu = <div>
+                <ul>
+                  <li>First Item </li>
+                  <li>Second Item </li>
+                  <li>Third Item </li>
+                </ul>
+              </div>
+    } else {
+      menu = "";
+    }
+    return (
+      <div id = "menu">
+        <i className = "fa fa-plus" onClick = { this.toggleMenu }/>
+      <ReactCSSTransitionGroup transitionName = "menu" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+        {menu}
+      </ReactCSSTransitionGroup>
+    </div>
+    )
+  }
+}
+
+
+module.exports = DropDown;
