449798a3d3eef713c7fe3f3221e070fa6364ede2
elgskred <christoffer.antonsen@gmail.com>
2017-10-18 13:42:04 +0200

    Tidying up class names and unused components

--- client/views/Edit.js
+++ client/views/Edit.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router';
-import Dropzone from 'react-dropzone';
-import DropzoneComponent from 'react-dropzone-component';
 import UploadHandler from '../components/UploadHandler';
 import Keyword from '../components/KeywordTags';
 import {Editor, EditorState, RichUtils, convertFromRaw, convertToRaw} from 'draft-js';
--- client/views/home.js
+++ client/views/home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router';
 import HeaderMenu from '../components/HeaderMenu';
-import PG from '../components/PG';
 import Slideshow from '../components/Slideshow';
 
 class Home extends React.Component {
