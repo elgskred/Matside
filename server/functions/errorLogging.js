8b9bc2112dc2a02a8e9378699c8d5e633d11ca20
elgskred <christoffer.antonsen@gmail.com>
2017-10-18 14:03:28 +0200

    Added errorlogging on the server

---
+++ server/functions/errorLogging.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = '../logs';
+const fileName = '/errlogs.txt';
+const newLine = '\n';
+
+
+
+exports.logToFile = function(data, callback) {
+	dt = new Date();
+	var yyyy = dateIn.getFullYear();
+	var mm = dateIn.getMonth()+1; // getMonth() is zero-based
+	var dd  = dateIn.getDate();
+	var hh = dateIn.getHours();
+	var mm = dateIn.getMinutes();
+	var timeStamp = hh+':'+mm+'-'+dd+'/'+mm+'/'+yyyy;
+
+
+	fs.mkdir(path, (err) => {
+	if (err) {
+		if (err.code === 'EEXIST') {
+			fs.open(path + fileName, 'wx', (err) => {
+				if (err) {
+					if (err.code === 'EEXIST') {
+						fs.appendFile(path + fileName, timeStamp + data + newLine, (err) => {
+							if (err) callback(err);
+						});
+					return;
+					}
+					throw err;
+				} else {
+					fs.appendFile(path + fileName, timeStamp + data + newLine, (err) => {
+						if (err) callback(err);
+					});
+				}
+			})
+		} else {
+
+		}
+	} else {
+		fs.appendFile(path + fileName, timeStamp + data + newLine, (err) => {
+			if (err) callback(err);
+		});
+	}
+	});
+}
+
+
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,8 @@ var async = require("async");
 var multer = require('multer');
 var bodyParser = require('body-parser');
 var functions = require('../functions/functions.js');
+var errLog = require('..functions/errorLogging.js');
+var util = require('util');
 const storage = multer.diskStorage({
     destination: './public/uploads/',
     filename: function (req, file, cb) {
@@ -28,30 +30,37 @@ var visitors = [];
 
 
 exports.recipe = function(req, res) {
-	console.log(req.body);
+  //Adds a new recipe to the db
   var UID;
+  //The table "recipes" holds the primary key, recipes are uploaded here first to get the UID.
   async.parallel([async.apply(functions.insertRecipe, req.body)],
     function done(err, results) {
       if (err) {
-        console.log(results);
+        errLog.writeToFile('Failed to insert recipe into table');
+        errLog.writeToFile(err);
       } else {
         UID = results[0];
-        console.log("recipe ok");
+        errLog.writeToFile(util.format('UID: %d received', UID));
+        console.log('UID: %d received', UID);
+        //Uses the UID as foregin key in the other tables
         async.parallel([async.apply(functions.insertIngredients, req.body, UID), async.apply(functions.insertPictures, req.body, UID), async.apply(functions.insertKeywords, req.body, UID)],
           function done(err, results) {
             if (err) {
+              errLog.writeToFile(err);
               console.log(err);
             } else {
-              console.log("rest ok");
+              errLog.writeToFile(util.format("Recipe with UID: %d uploaded correctly", UID));
+              console.log("Recipe with UID: %d uploaded correctly", UID);
             }
           });
       }
-      res.send("ok");
+      res.send(util.format("Recipe with UID: %d uploaded correctly", UID));
     })
 	
 }
 
 exports.search = function(req, res) {
+  //Search for recipes based on name or ingredients
   console.log("search");
   var str = req.params.id;
   var includes = [];
@@ -75,7 +84,8 @@ exports.search = function(req, res) {
     };
     }, function(err) {
       if (err) {
-
+        errLog.writeToFile('Failed to split search strings');
+        errLog.writeToFile(err);
       } else {
       };
     })
@@ -83,12 +93,14 @@ exports.search = function(req, res) {
 	async.parallel([async.apply(functions.searchSentence, searchSentence.substring(1)), async.apply(functions.searchTerm, searchTerm), async.apply(functions.searchIncludes, includes), async.apply(functions.searchExcludes, excludes)],
 		function done (err, results) {
 			if (err) {
-				console.log(err);
+        errLog.writeToFile('Error while searching for recipes based on name or ingredients');
+        errLog.writeToFile(err);
 			};
-      console.log("search results");
+      console.log("Results sendt to client");
 			res.send(results);
 		});
 }
+
 //Retrieves the recipes by looking for a specific UID
 exports.recipes = function(req, res) {
   var v = req.connection.remoteAddress
@@ -96,11 +108,14 @@ exports.recipes = function(req, res) {
   var visitor = {ip: v, page: uid};
   var recordedVisit = false;
   var flag = false;
+  //Increment the view counter if the IP is unique
   if (visitors.length == 0) {
     visitors.push(visitor);
     async.parallel([async.apply(functions.incrementViews,uid)], 
         function done (err, results) {
           if (err) {
+            errLog.writeToFile('Failed to increment views');
+            errLog.writeToFile(err);
             console.log(err);
           };
         });
@@ -114,12 +129,16 @@ exports.recipes = function(req, res) {
     }
   }, function(err) {
     if(err) {
+      errLog.writeToFile('Failed to loop through unique visitors');
+      errLog.writeToFile(err);
       console.log(err);
     } else {
       if (recordedVisit == false) {
         async.parallel([async.apply(functions.incrementViews,uid)], 
         function done (err, results) {
           if (err) {
+            errLog.writeToFile('Failed to increment views');
+            errLog.writeToFile(err);
             console.log(err);
           };
         });
@@ -128,12 +147,16 @@ exports.recipes = function(req, res) {
       };
     }
   });
-
+  //Gets the recipe, ingredients, images and keywords based on the requested UID
+  console.log("Getting recipe with UID %d", uid);
   async.parallel([async.apply(functions.searchRecipeByUID, uid), async.apply(functions.searchIngredientsByUID, uid), async.apply(functions.searchPictureByUID, uid), async.apply(functions.searchKeywordsByUID, uid)],
     function done (err, results) {
       if (err) {
+        errLog.writeToFile('Failed to retreive recipe with given UID');
+        errLog.writeToFile(err);
         console.log(err);
       };
+      console.log("Results for UID: %d retreived", uid);
       res.send(results);
     });
 }
@@ -145,35 +168,45 @@ exports.uploadHandler = function(req, res) {
 	}
 	res.send(req.file.path); //Sends the file path back to the user so the image can be associated with the correct recipe
 }
+
 //Finds the images tied  to a specific UID
 exports.searchImg = function(req, res) {
   var temp = [];
+  console.log("Get images with UID: %d", req.body.UID);
   async.parallel([async.apply(functions.searchPicturesByUID, req.body.UID)],
     function done (err, results) {
       if (err) {
+        errLog.writeToFile('Failed to find images tied  to specific UID');
+        errLog.writeToFile(err);
         console.log(err);
       };
+      console.log("Retreived images with UID: %d", req.body.UID);
       res.send(results[0]);
     });
 }
 
-//Returns the current popular recipes
+//Returns the current top 5 popular recipes
 exports.popularRecipes = function(req, res) {
   async.parallel([async.apply(functions.getPopularRecipes)],
     function done (err, results) {
       if (err) {
+        errLog.writeToFile('Failed to find popular recipes');
+        errLog.writeToFile(err);
         console.log(err);
       }
-      console.log(results[0]);
+      console.log("Get popular recipes");
       res.send(results[0]);
     })
 }
 
 //Updates a recipe with a given UID
 exports.updateRecipe = function(req, res) {
+  console.log("Update recipe with UID: %d", req.body.UID);
   async.parallel([async.apply(functions.updateRecipe, req.body), async.apply(functions.updateIngredients, req.body), async.apply(functions.updateImages, req.body), async.apply(functions.updateKeywords, req.body)],
     function done (err, results) {
       if (err) {
+        errLog.writeToFile('Failed to update recipe');
+        errLog.writeToFile(err);
         console.log(err);
       }
       console.log(results);
